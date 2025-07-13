73bb22c7010e269155c7574f32e7189cbb2320b2
Puth Sovannary <naryputh0401@gmail.com>
2025-07-13 19:54:41 +0700

    add prod

--- api/products.php
+++ api/products.php
@@ -7,17 +7,17 @@ header('Access-Control-Allow-Headers: Content-Type');
 $products = [
     [
         "id" => 1,
-        "name" => "Classic White T-Shirt",
+        "name" => "Classic Dress",
         "price" => 29.99,
         "description" => "Premium cotton t-shirt perfect for everyday wear",
-        "image" => "https://i.pinimg.com/1200x/dc/3e/b4/dc3eb40f49b8823f27320ab75fecc969.jpg"
+        "image" => "https://i.pinimg.com/1200x/c3/08/b8/c308b8715d88ca87b8fbe0d19b0da85e.jpg"
     ],
     [
         "id" => 2,
-        "name" => "Blue Denim Jacket",
-        "price" => 79.99,
+        "name" => "Blue Denim Dress",
+        "price" => 69.99,
         "description" => "Stylish denim jacket for casual wear",
-        "image" => "https://i.pinimg.com/736x/4e/b4/26/4eb426d7351e4dc20afed8bcc64c3d82.jpg"
+        "image" => "https://i.pinimg.com/1200x/6f/d4/c6/6fd4c624ce6e6ad2d08aa6a5d4281e34.jpg"
     ],
     [
         "id" => 3,
@@ -25,13 +25,41 @@ $products = [
         "price" => 89.99,
         "description" => "Elegant black dress for special occasions",
         "image" => "https://i.pinimg.com/736x/f9/bc/3d/f9bc3ddaaa35922fe33989a97a96b347.jpg"
-    ]
-     [
+    ],
+    [
         "id" => 4,
         "name" => "Classic Denim Dress",
         "price" => 49.99,
         "description" => "Elegant black dress for special occasions",
         "image" => "https://i.pinimg.com/1200x/c3/08/b8/c308b8715d88ca87b8fbe0d19b0da85e.jpg"
+    ],
+    [
+        "id" => 5,
+        "name" => "Elegant Summer Dress",
+        "price" => 49.99,
+        "description" => "Lightweight and breezy, perfect for warm days and special occasions.",
+        "image" => "https://i.pinimg.com/736x/3a/aa/f0/3aaaf0707c98512b3d9fb95d0c176fcd.jpg"
+    ],
+    [
+        "id" => 6,
+        "name" => "Women's Knit Sweater",
+        "price" => 29.99,
+        "description" => "Soft cotton shirt with a modern fit. Ideal for both work and play.",
+        "image" => "https://i.pinimg.com/736x/7d/80/b9/7d80b9afe4f1e97263ca6f797fe79f10.jpg"
+     ],
+    [
+         "id" => 7,
+         "name" => "Slim Fit Shirt",
+         "price" => 39.99,
+         "description" => "Comfortable and stylish shirt for everyday wear.",
+         "image" => "https://i.pinimg.com/736x/44/bd/27/44bd279b1e8c5aae003bac5618e83f59.jpg"
+    ],
+    [
+         "id" => 8,
+         "name" => "Women's Knit Sweater",
+         "price" => 199.99,
+         "description" => "Cozy and stylish knit sweater, available in multiple colors.",
+         "image" => "https://i.pinimg.com/736x/7d/80/b9/7d80b9afe4f1e97263ca6f797fe79f10.jpg"
     ]
 ];
 
